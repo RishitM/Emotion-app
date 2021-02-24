3a1fff7be3a48096db61b6c0fc8afb3dc7269a57
RishitM <72683716+RishitM@users.noreply.github.com>
2021-02-24 19:40:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -33,6 +33,40 @@ function got_Result(error,result){
     Name2=result[1].label;
        document.getElementById("name1").innerHTML= Name1;
        document.getElementById("name2").innerHTML= Name2;
-
+       
+       if (Name1 == "happy"){
+           document.getElementById("emoji1").innerHTML="&#128512;"; 
+       }
+       if (Name1 == "Sad"){
+        document.getElementById("emoji1").innerHTML="&#128532;"; 
+    }
+    if (Name1 == "Angry"){
+        document.getElementById("emoji1").innerHTML="&#128545;"; 
+    }
+    if (Name1 == "Crying"){
+        document.getElementById("emoji1").innerHTML="&#128546;"; 
+    }
+    if (Name2 == "happy"){
+        document.getElementById("emoji2").innerHTML="&#128512;"; 
+    }
+    if (Name2 == "Sad"){
+     document.getElementById("emoji2").innerHTML="&#128532;"; 
+ }
+ if (Name2 == "Angry"){
+     document.getElementById("emoji2").innerHTML="&#128545;"; 
+ }
+ if (Name2 == "Crying"){
+     document.getElementById("emoji2").innerHTML="&#128546;"; 
+ }
+ speak()
    }
+
+}
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data1="I think either you are,"+ Name1;
+    speak_data2=",or you are"+ Name2;
+    var utterThis= new SpeechSynthesisUtterance(speak_data1+speak_data2) ;
+    utterThis.rate=0.7;
+    synth.speak(utterThis);
 }
